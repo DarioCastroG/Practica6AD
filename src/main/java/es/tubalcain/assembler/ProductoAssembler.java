e6431de41b8311ba30ec1fe2d2adc761cbf9f09e
Dario <dariojoaquin.castro.gutierrez@iestubalcain.net>
2026-01-09 13:22:32 +0100

    Ver 2.1.2

--- src/main/java/es/tubalcain/assembler/ProductoAssembler.java
+++ src/main/java/es/tubalcain/assembler/ProductoAssembler.java
@@ -1,4 +1,48 @@
 package es.tubalcain.assembler;
 
+import es.tubalcain.domain.Producto;
+import es.tubalcain.dto.ProductoDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductoAssembler {
+
+    public ProductoDTO toDto(Producto producto) {
+        if (producto == null) return null;
+
+        ProductoDTO dto = new ProductoDTO();
+
+        dto.setId(producto.getId());
+        dto.setNombre(producto.getNombre());
+        dto.setDescripcion(producto.getDescripcion());
+        dto.setPrecio(producto.getPrecio());
+        dto.setCategoria(producto.getCategoria());
+        dto.setStock(producto.getStock());
+
+        return dto;
+    }
+
+    public Producto toEntity(ProductoDTO dto) {
+        if (dto == null) return null;
+
+        Producto producto = new Producto();
+        mapCommonFields(dto,producto);
+
+        return producto;
+    }
+
+    public void updateEntity(ProductoDTO dto, Producto producto) {
+        if (dto == null) return;
+
+        mapCommonFields(dto,producto);
+    }
+
+    public void mapCommonFields(ProductoDTO dto, Producto producto) {
+        producto.setId(dto.getId());
+        producto.setNombre(dto.getNombre());
+        producto.setDescripcion(dto.getDescripcion());
+        producto.setPrecio(dto.getPrecio());
+        producto.setCategoria(dto.getCategoria());
+        producto.setStock(dto.getStock());
+    }
 }
